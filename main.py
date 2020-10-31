629daba55854e15f395e3b2db8b723e3a79f7ea7
Philipp Schneider <philipp@ifp.uni-stuttgart.de>
2020-10-31 17:58:13 +0100

    switched to enu crf

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ def zero2sun(lat0, lon0, alt0, date):
     azi=[]
     srange=[]
     for date_single in date:
-        ele.append( get_altitude(lat0, lon0, date_single))
-        azi.append( get_azimuth( lat0, lon0, date_single) )
-        srange.append(150e6)
+        ele.append( get_altitude(lat0, lon0, date_single,alt0))
+        azi.append( get_azimuth( lat0, lon0, date_single,alt0) )
+        srange.append(150e9)
         
     
     print('Elevation=',ele)
@@ -46,6 +46,41 @@ def zero2sun(lat0, lon0, alt0, date):
     
     return(x_obs,y_obs,z_obs,x_sun,y_sun,z_sun)
 
+def zero2sun_enu(lat0, lon0, alt0, date):
+    """
+    This functions calculates the suns Euclidean coordinates (ecef) based on a time.
+    It also returns the coordiantes of an observers in the same reference frame
+    
+    Input:
+        lat0: latitude observers
+        lon0: longitude observer
+        alt0: hightAbove ellipsoid observer
+        date: dateTime of interest
+    Output:
+        x_obs,y_obs,z_obs: enu coordinates of the observer = [0 0 0]
+        x_sun,y_sun,z_sun: enu coordinates of the sun at dateTime       
+    """
+    from pysolar.solar import get_altitude,get_azimuth
+    import pymap3d as pm
+    import numpy as np
+    
+    ele=[]
+    azi=[]
+    srange=[]
+    for date_single in date:
+        ele.append( get_altitude(lat0, lon0, date_single,alt0))
+        azi.append( get_azimuth( lat0, lon0, date_single,alt0) )
+        srange.append(150e9)
+        
+    
+    print('Elevation=',ele)
+    print('Azimuth=',azi)
+    
+    e_obs,n_obs,u_obs = 0,0,0
+    e_sun,n_sun,u_sun = pm.aer2enu(azi,ele,srange)
+    
+    
+    return(e_obs,n_obs,u_obs,e_sun,n_sun,u_sun)
 
 
 #  main code starts here 
@@ -60,15 +95,18 @@ import numpy as np
 # define a dateTime
 date_base = datetime.datetime.now(pytz.UTC)
 date_base = datetime.datetime(2000, 3, 21,12,tzinfo=pytz.UTC)
-date = [date_base + datetime.timedelta(days=i) for i in range(0,182)]
+date = [date_base + datetime.timedelta(days=i) for i in range(0,182)] # day steps
+#date = [date_base + datetime.timedelta(hours=i) for i in range(0,12)] # hour steps
+
 
 # define a observers Point
-lat0=48.7758
-lon0=9.1829
+lat0=0#48.7758
+lon0=0#9.1829
 alt0=500
 
 
-x_obs,y_obs,z_obs,x_sun,y_sun,z_sun=zero2sun(lat0, lon0,alt0, date)
+#x_obs,y_obs,z_obs,x_sun,y_sun,z_sun=zero2sun(lat0, lon0,alt0, date)
+x_obs,y_obs,z_obs,x_sun,y_sun,z_sun=zero2sun_enu(lat0, lon0,alt0, date)
 
 
 import math
@@ -79,7 +117,7 @@ from scipy.spatial.transform import Rotation as R
 # generate scale
 radius = 0.3 #meter
 Nmins=12*60 # number of minutes on scale
-t=np.linspace(-math.pi/2, math.pi/2, num=Nmins) # create parameters
+t=np.linspace(0, math.pi, num=Nmins) # create parameters
 #paremetrize half a circle
 x = radius*np.cos(t)
 y = radius*np.sin(t)
@@ -87,19 +125,25 @@ z = np.zeros([Nmins,])
 
 
 # generate gnomon
-xg=np.array([0, 0])+x_obs
-yg=np.array([0, 0])+y_obs
-zg=np.array([0, 0 + radius])+z_obs
+xg=np.array([0, 0])
+yg=np.array([0, 0])
+zg=np.array([0- radius, 0 + radius])
 
 #transpose circle so the normal vec face polar star
-r = R.from_euler('z',180, degrees=True)
+r = R.from_euler('z',0, degrees=True)
 xyz=r.apply(np.array([x,y,z]).transpose([1,0]))
 
-r = R.from_euler('y',lat0, degrees=True)
+r = R.from_euler('x',-(90-lat0), degrees=True)
+
+xyzg=r.apply(np.array([xg,yg,zg]).transpose([1,0]))
 
 xyz=r.apply(xyz)
 
 # translate circle to observes position
+xg=xyzg[:,0]+x_obs
+yg=xyzg[:,1]+y_obs
+zg=xyzg[:,2]+z_obs
+
 x=xyz[:,0]+x_obs
 y=xyz[:,1]+y_obs
 z=xyz[:,2]+z_obs
@@ -110,7 +154,7 @@ min= int(np.floor(Nmins/2)) # reference minute
 
 vectors=np.array([[x_sun-x[min]],[y_sun-y[min]],[z_sun-z[min]]])  # vector = X-X_sun
 
-vectors /= np.sqrt((vectors ** 2).sum(-1))[..., np.newaxis] # norm vector
+vectors /= np.linalg.norm(vectors[:,0,0]) # norm vector by deviding by length of first
 
 
 # get x y z  of normed vector
@@ -126,14 +170,28 @@ nz=vectors[2,].flatten()
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for i in range(0,nx.size-1):
-    ax.plot([x[min], x[min]+10*radius*nx[i]],[y[min], y[min]+10*radius*ny[i]],[z[min], z[min]+10*radius*nz[i]],'-')
+    ax.plot([x[min], x[min]+radius*nx[i]],[y[min], y[min]+radius*ny[i]],[z[min], z[min]+radius*nz[i]],'-')
 ax.plot(x,y,z,'.')
 
 #draw gnomon
 ax.plot(xg,yg,zg,'-')
 
 
-ax.axis('auto')
+
+
+# Create cubic bounding box to simulate equal aspect ratio
+max_range = 0.3
+Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() 
+Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() 
+Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() 
+# Comment or uncomment following both lines to test the fake bounding box:
+for xb, yb, zb in zip(Xb, Yb, Zb):
+   ax.plot([xb], [yb], [zb], 'w')
+
+ax.set_xlabel('East')
+ax.set_ylabel('North')
+ax.set_zlabel('Up')
+ax.set_aspect('auto')
 #print('sun=',x_sun,y_sun,z_sun)
 
 # fig = plt.figure()
